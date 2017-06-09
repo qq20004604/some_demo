49bde3be125894824ab1d387c052796132f21fa7
王冬 <20004604@qq.com>
2017-06-10 03:20:34 +0800

    更新

--- 五子棋/Backgammon.js
+++ 五子棋/Backgammon.js
@@ -2,23 +2,24 @@
  * Created by 王冬 on 2017/6/9.
  */
 
-var config = {
+let config = {
     imgSize: 39
 }
 
-var info = {
+let info = {
     size: 19,
     first: "black",
     // 下棋的每一步都存在这里，索引为0的地方，是棋盘(1, 1)的位置；
-    // html标签的索引值var index = info.steps[info.steps.length - 1] + 1;
+    // html标签的索引值let index = info.steps[info.steps.length - 1] + 1;
     steps: [],
     gameover: false
 }
 
 function start(isCreate) {
+    let size = 0;
     if (isCreate) {
         //创建棋盘
-        var size = Number($("#size").value);
+        size = Number($("#size").value);
         info.size = size;
     } else {
         size = info.size
@@ -74,8 +75,8 @@ $ = function (selector) {
 
 //创建<div class="box"></div>
 //参数是数组，是额外添加的类名
-var createBox = function (classNames, index) {
-    var box = document.createElement("div");
+let createBox = function (classNames, index) {
+    let box = document.createElement("div");
     box.classList.add("box");
     if (classNames) {
         classNames.forEach(item => {
@@ -89,8 +90,8 @@ var createBox = function (classNames, index) {
 }
 
 //创造棋子
-var createPiece = function (index) {
-    var piece = document.createElement("div");
+let createPiece = function (index) {
+    let piece = document.createElement("div");
     piece.classList.add("piece");
     //奇数步为黑
     if ((info.steps.length + 1) % 2 === 1) {
@@ -111,24 +112,24 @@ $("#checkerboard").onclick = function (evt) {
     if (info.gameover) {
         return;
     }
-    var dom = evt.target;
-    var index = dom.getAttribute("index") - 1;
+    let dom = evt.target;
+    let index = dom.getAttribute("index") - 1;
     if (checkCanPutPiece(index)) {
         createPiece(index);
     } else {
         return;
     }
     //如果有胜利者
-    var isWin = whoIsWinner();
+    let isWin = whoIsWinner();
     if (isWin) {
         info.gameover = true;
-        var winner = info.steps.length % 2 ? "黑方" : "白方";
+        let winner = info.steps.length % 2 ? "黑方" : "白方";
         $("#winner").innerHTML = winner + "胜利！<br>" + isWin;
     }
 }
 
 //判断是否可以下这一步棋
-var checkCanPutPiece = function (index) {
+let checkCanPutPiece = function (index) {
     if (info.steps.indexOf(index) > -1) {
         return false;
     } else {
@@ -137,15 +138,15 @@ var checkCanPutPiece = function (index) {
 }
 
 //悔棋
-var backPiece = function () {
-    var index = info.steps.pop();
-    var child = $(".box")[index].children[0];
+let backPiece = function () {
+    let index = info.steps.pop();
+    let child = $(".box")[index].children[0];
     $(".box")[index].removeChild(child);
     changeColor(index);
 }
 
 //切换颜色
-var changeColor = function () {
+let changeColor = function () {
     if ((info.steps.length + 1) % 2 === 1) {
         $("#thisOrder").innerHTML = "<span style='background-color:white;color:black;display:inline-block;width:100%;'>黑方</span>"
     } else {
@@ -154,18 +155,18 @@ var changeColor = function () {
 }
 
 //胜负判断
-var whoIsWinner = function () {
+let whoIsWinner = function () {
     //先获取最后一步的索引
-    var index = info.steps[info.steps.length - 1];
-    var rect = rectTransformation.changeIndexToXY(index);
+    let index = info.steps[info.steps.length - 1];
+    let rect = rectTransformation.changeIndexToXY(index);
     //以该位置为中心，下来取横、竖、左上到右下、右上到左下四个数组
     //每个数组长度为9，当前索引为第5个元素
-    var Left_Right = [];
-    var Top_Bottom = [];
-    var TopLeft_BottomRight = [];
-    var TopRight_BottomLeft = [];
+    let Left_Right = [];
+    let Top_Bottom = [];
+    let TopLeft_BottomRight = [];
+    let TopRight_BottomLeft = [];
     //从左到右，序数每次增加 1
-    for (var i = -4; i < 5; i++) {
+    for (let i = -4; i < 5; i++) {
         Left_Right.push({
             x: rect.x + i,
             y: rect.y
@@ -195,7 +196,7 @@ var whoIsWinner = function () {
     TopLeft_BottomRight = filter.filterSameColor(TopLeft_BottomRight);
     TopRight_BottomLeft = filter.filterSameColor(TopRight_BottomLeft);
 
-    var haveWin = "";
+    let haveWin = "";
     if (isWin(Left_Right)) {
         haveWin += "左右一排五子连珠，";
     }
@@ -217,11 +218,11 @@ var whoIsWinner = function () {
 }
 
 //x、y坐标与索引转换函数
-var rectTransformation = {
+let rectTransformation = {
     changeIndexToXY  (index) {
         //index从0开始，左上角坐标为(1,1)
-        var x = index % info.size + 1;
-        var y = parseInt(index / info.size) + 1;
+        let x = index % info.size + 1;
+        let y = parseInt(index / info.size) + 1;
         return {
             x, y
         }
@@ -233,7 +234,7 @@ var rectTransformation = {
 
 
 //过滤器，用于处理越界情况
-var filter = {
+let filter = {
     //超界排除
     beyondBorder(arr){
         //原理：x或y小于1的，或者x或y大于info.size（比如19格棋盘就是>19）的
@@ -256,13 +257,13 @@ var filter = {
         // 3、查看该元素是否在已下棋的数组中info.steps，不在则设置该位置为undefined；
         // 4、如果在，则查看该位置与当前这一步下棋步数的奇偶是否一致，不一致则设置该位置为undefined；
         // 5、如果在info.steps，并且奇偶一致，则保持不变；
-        var isEven = (info.steps.length - 1) % 2;
+        let isEven = (info.steps.length - 1) % 2;
         return arr.map(item => {
             if (item === undefined) {
                 return undefined;
             }
-            var index = rectTransformation.changeXYToIndex(item);
-            var i = info.steps.indexOf(index);
+            let index = rectTransformation.changeXYToIndex(item);
+            let i = info.steps.indexOf(index);
             if (i === -1 || i % 2 !== isEven) {
                 //不存在
                 return undefined;
@@ -274,13 +275,13 @@ var filter = {
 }
 
 //判断是否胜利
-var isWin = function (arr) {
+let isWin = function (arr) {
     // 参数是一个排除掉过界、以及颜色不同的对象之后的数组，只有当前位置元素合法才保留该位置元素，否则为undefined。
     // 遍历数组，当前元素不为空则放入临时数组，临时数组元素个数满5则说明符合条件；
     // 为空则清除临时数组；
     // 遍历结束后，未曾满5过，则说明这一排不成立。
-    var tempArr = [];
-    var overFive = false;
+    let tempArr = [];
+    let overFive = false;
     arr.forEach(function (item) {
         if (item !== undefined) {
             tempArr.push(item);
@@ -293,7 +294,6 @@ var isWin = function (arr) {
     })
     return overFive;
 }
-
 
 //启动棋盘
 start(true);
