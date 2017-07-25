3f45b9efa7fcc294bd7bc173b67653c1c8d7c33f
王冬 <20004604@qq.com>
2017-07-25 10:54:43 +0800

    开始更新Promise

---
+++ 仿jquery选择器$/$.js
@@ -0,0 +1,26 @@
+﻿(function (f) {
+    f();
+})(function () {
+    //选择器
+    if (window.$ !== "undefined") {
+        var $OBJECT = window.$;
+    }
+    window.$ = function (selector) {
+        if (selector.charAt(0) === '#') {
+            return document.querySelector(selector)
+        } else {
+            return document.querySelectorAll(selector);
+        }
+    }
+    if ($OBJECT) {
+        if (Object.assign) {
+            Object.assign(window.$, $OBJECT)
+        } else if (Object.setPrototypeOf) {
+            Object.setPrototypeOf(window.$, $OBJECT);
+        } else if (window.$.__proto__) {
+            window.$.__proto__ = $OBJECT;
+        } else {
+            console.error("你的浏览器不支持$选择器（这种可能性太低了……）");
+        }
+    }
+});
