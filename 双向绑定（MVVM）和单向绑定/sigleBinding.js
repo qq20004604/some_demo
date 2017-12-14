b928878ff2954b716c66491aa958fa5a97083a98
王冬 <20004604@qq.com>
2017-12-14 17:51:11 +0800

    修了一个bug

--- 双向绑定（MVVM）和单向绑定/sigleBinding.js
+++ 双向绑定（MVVM）和单向绑定/sigleBinding.js
@@ -4,15 +4,19 @@
  * weChat: qq20004604
  */
 
-// 单向绑定
+// 这是一个简陋的单向绑定
 // DOM需要是jQuery的DOM（为了省事）
 const sigleBinding = {
     // 绑定文本框
     DOM(obj, key, $DOM) {
+        let state = Object.getOwnPropertyDescriptor(obj, key)
         Object.defineProperty(obj, key, {
             configurable: true,
             set(newVal) {
                 $DOM.text(newVal)
+                if (state && typeof state.get === 'function') {
+                    state.get.call(null, newVal)
+                }
             },
             get() {
                 return $DOM.text()
@@ -21,10 +25,14 @@ const sigleBinding = {
     },
     // 绑定输入框
     input(obj, key, $input) {
+        let state = Object.getOwnPropertyDescriptor(obj, key)
         Object.defineProperty(obj, key, {
             configurable: true,
             set(newVal) {
                 $input.val(newVal)
+                if (state && typeof state.get === 'function') {
+                    state.get.call(null, newVal)
+                }
             },
             get() {
                 return $input.val()
