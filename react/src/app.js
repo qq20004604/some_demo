043de7102bacfe3303dbf3a3a521f6dfd91f7677
王冬 <20004604@qq.com>
2018-01-15 11:30:23 +0800

    简单教程快速更新

--- react/src/app.js
+++ react/src/app.js
@@ -9,12 +9,15 @@ let domClass = 'container parent'
 
 class HelloWord extends React.Component {
     constructor(props) {
+        // props的值就是你传给他的变量，比如这里就是 {toChild: 'world'}
         super(props);
+        // 必须存在this.state中
         this.state = {
-            props: props,
+            world: props.toChild,
             seconds: 0
         }
         setInterval(() => {
+            // 调用setState方法修改变量的值
             this.setState({
                 seconds: this.state.seconds + 1
             })
@@ -23,17 +26,20 @@ class HelloWord extends React.Component {
 
     render() {
         return <div className={domClass}>
-            你好，{this.state.props.user}距离上一次修改页面，过去了{this.state.seconds}秒
+            {/* 需要通过this.state.world 来使用。当然你也可以赋值到 this 的其他变量 */}
+            Hello，{this.state.world}！距离上一次修改页面，过去了{this.state.seconds}秒
         </div>
     }
 }
 
-let abc = 'abc'
+// 要传的变量
+let foo = 'world'
 
 ReactDOM.render(
     <div>
-        <Foo></Foo>
-        {/*<HelloWord user={abc}/>*/}
+        {/* ---- toChild 就是传递给子组件的变量的key ---- */}
+        {/* ---- foo就是被传的变量（这里就是字符串 'world'） ---- */}
+        <HelloWord toChild={foo}/>
         <p>当前时间是：{formatDate(new Date())}</p>
         {/*<Leaner/>*/}
     </div>,
